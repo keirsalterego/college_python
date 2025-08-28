ee6e715e0eddf2266cd90635e22c80dfeb053fbc
keir <darksalterego99@gail.com>
2025-08-28 21:12:12 +0530

    refactor: minor change in `q7.py`

--- assign-6/q7.py
+++ assign-6/q7.py
@@ -1,10 +1,10 @@
 
-sampleDict = {'a': 100, 'b': 200, 'c': 300}
+sampleDict = {'a': 100, 'b': 300, 'c': 400}
 
 
-value_to_check = 200
+# value_to_check = 200
 
-
+value_to_check = 100
 if value_to_check in sampleDict.values():
     print(f"{value_to_check} exists in the dictionary")
 else:
